6f65b319ce07785ab9f2ebf41a250e0b0adb0811
KrisdaC <chugh.krisda@gmail.com>
2025-01-21 10:40:53 +0700

    update GA4

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import MainRouter from "./MainRouter"; // Import MainRouter
+import MainRouter from "./MainRouter";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-BVNCPL0HKC"); // Replace with your GA Measurement ID
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
